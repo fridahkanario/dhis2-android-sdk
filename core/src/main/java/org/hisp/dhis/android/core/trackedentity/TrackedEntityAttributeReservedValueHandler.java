53b8bec9631c4c3e294a2da89148f7f4def96b46
Luis Delgado Romera <luisyng@gmail.com>
2018-04-29 19:59:17 +0200

    reserved-value-handler: rewrite handler without extension

--- core/src/main/java/org/hisp/dhis/android/core/trackedentity/TrackedEntityAttributeReservedValueHandler.java
+++ core/src/main/java/org/hisp/dhis/android/core/trackedentity/TrackedEntityAttributeReservedValueHandler.java
@@ -29,20 +29,15 @@ package org.hisp.dhis.android.core.trackedentity;
 
 import org.hisp.dhis.android.core.common.GenericHandler;
 import org.hisp.dhis.android.core.common.ObjectWithoutUidHandlerImpl;
-import org.hisp.dhis.android.core.common.ObjectWithoutUidStore;
 import org.hisp.dhis.android.core.data.database.DatabaseAdapter;
 
-@SuppressWarnings("PMD.UseUtilityClass")
-public final class TrackedEntityAttributeReservedValueHandler extends
-        ObjectWithoutUidHandlerImpl<TrackedEntityAttributeReservedValue, TrackedEntityAttributeReservedValueModel> {
+public final class TrackedEntityAttributeReservedValueHandler {
 
-    TrackedEntityAttributeReservedValueHandler(ObjectWithoutUidStore<TrackedEntityAttributeReservedValueModel> store) {
-        super(store);
-    }
+    private TrackedEntityAttributeReservedValueHandler () {}
 
     public static GenericHandler<TrackedEntityAttributeReservedValue, TrackedEntityAttributeReservedValueModel>
     create(DatabaseAdapter databaseAdapter) {
-        return new TrackedEntityAttributeReservedValueHandler(
+        return new ObjectWithoutUidHandlerImpl<>(
                 TrackedEntityAttributeReservedValueStore.create(databaseAdapter));
     }
 }
